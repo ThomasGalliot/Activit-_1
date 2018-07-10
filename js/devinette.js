58fa481a395bc2b150bf4f7f37afb383e3231805
ThomasGalliot <thomas.galliot@hotmail.fr>
2018-07-10 19:14:09 +0200

    Activité1

--- js/devinette.js
+++ js/devinette.js
@@ -14,3 +14,29 @@ var solution = Math.floor(Math.random() * 100) + 1;
 //console.log("(La solution est " + solution + ")");
 
 // TODO : complétez le programme
+var resultat = false;
+var nbrEssais = 0;
+
+while (resultat === false && nbrEssais === 6){
+    var nbr = prompt("entré un nombre entre 1 et 100");
+    nbr = parseInt(nbr);
+    nbrEssais++;
+ 
+    if (nbr < 1 || nbr > 100)
+        alert("vous n'avez pas entré un nombre entre 1 et 100, veuiller recommencé");
+    
+    else if (nbrEssais === 6)
+        alert("Vous avez perdu, le nombre était " + solution); 
+
+    
+    else if (nbr < solution)
+        alert(nbr + " est trop petit");
+    
+    else if (nbr > solution)
+        alert(nbr + " est trop grand");
+    
+    else if (nbr === solution && nbrEssais <= 6){
+        alert("Bravo ! Vous aves trouver le nombre en " + nbrEssais + " essais la solution était " + solution);
+        resultat = true;
+    }
+}
